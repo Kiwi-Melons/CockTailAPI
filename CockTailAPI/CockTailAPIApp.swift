1a15f6b472b5685b5424a46c7b670f3b02cc2870
Kiwi-Melons <joycewu548@gmail.com>
2024-12-16 15:11:01 -0500

    Initial Commit

---
+++ CockTailAPI/CockTailAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CockTailAPIApp.swift
+//  CockTailAPI
+//
+//  Created by Joyce Wu (student LM) on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CockTailAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CockTailAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CockTailAPI
+//
+//  Created by Joyce Wu (student LM) on 12/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
