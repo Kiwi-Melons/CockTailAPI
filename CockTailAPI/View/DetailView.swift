99d7549c5368e57c5a92cb4f37356c63e654c287
Kiwi-Melons <joycewu548@gmail.com>
2025-01-12 20:44:09 -0500

    Added some randos

---
+++ CockTailAPI/View/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  CockTailAPI
+//
+//  Created by Joyce Wu (student LM) on 1/10/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ CockTailAPI/View/Navigation.swift
@@ -0,0 +1,18 @@
+//
+//  Navigation.swift
+//  CockTailAPI
+//
+//  Created by Joyce Wu (student LM) on 1/10/25.
+//
+
+import SwiftUI
+
+struct Navigation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Navigation()
+}
